cd58b4642a09acfd0bf5ddc58f2079c503a7a1b0
Jordan Poles <jpdev.noreply@gmail.com>
2023-05-22 18:57:07 -0400

    Deskeletonizing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,18 @@
 <script lang='ts'>
 	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
-	import '../app.postcss';
+	import '../app.css';
+
 
 	import Icon from '@iconify/svelte';
 	import { pb } from '$lib/pocketbase'
-
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-
+	import Header from './header.svelte';
 	$: auth = pb.authStore
 </script>
 
 
-<AppShell>
-	<svelte:fragment slot="header">
-		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-			<svelte:fragment slot="lead"><Icon icon="ph:robot-fill" height=40/></svelte:fragment>
-			<h2 class="h2 hover:variant-ghost duration-500 p-2 rounded-md"><a href="/">AI OOPS</a></h2>
-			<svelte:fragment slot="trail">
-				<a href="/submit" class="hover:variant-ghost variant-glass duration-500 p-2 rounded-md"> Submit  <Icon icon="mdi:plus-circle" inline={true} class="inline"/></a>
-				{#if !auth.isValid}<a href="/login" class="hover:variant-ghost variant-glass duration-500 p-2 rounded-md">Login</a>
-				{:else}<a href="/logout" class="hover:variant-ghost variant-glass duration-500 p-2 rounded-md">Logout</a>{/if}
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<slot />
-</AppShell>
+<div class="bg-neutral w-full h-full">
+	<Header />
+	<slot/>
+</div>
---
+++ src/routes/header.svelte
@@ -0,0 +1,40 @@
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    </div>
+    <div class="flex-none">
+      <div class="dropdown dropdown-end">
+        <label tabindex="0" class="btn btn-ghost btn-circle">
+          <div class="indicator">
+            <span class="badge badge-sm indicator-item">8</span>
+          </div>
+        </label>
+        <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+          <div class="card-body">
+            <span class="font-bold text-lg">8 Items</span>
+            <span class="text-info">Subtotal: $999</span>
+            <div class="card-actions">
+              <button class="btn btn-primary btn-block">View cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="dropdown dropdown-end">
+        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </label>
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <li>
+            <a class="justify-between">
+              Profile
+              <span class="badge">New</span>
+            </a>
+          </li>
+          <li><a>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
